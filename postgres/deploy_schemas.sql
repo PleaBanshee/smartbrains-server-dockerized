8d9301ca90cc03d06b69273210b1a6146c707952
PleaBanshee <llewellynant@gmail.com>
2022-02-02 13:53:31 +0200

    Refactored code

--- postgres/deploy_schemas.sql
+++ postgres/deploy_schemas.sql
@@ -1,4 +1,4 @@
 -- Deploy fresh database tables
 -- \i : execute scripts
-\i '/docker-entrypoint.initdb.d/tables/users.sql';
-\i '/docker-entrypoint.initdb.d/tables/login.sql';
+\i '/docker-entrypoint-initdb.d/tables/users.sql';
+\i '/docker-entrypoint-initdb.d/tables/login.sql';
